8af8c031327d6dd4797e385556d08a744ae577aa
uaaaaaaaa.com@gmail.com <ffkmf@r.vodafone.ne.jp>
2022-12-04 21:40:57 +0900

    フォントPATHなおした

--- lgtm/drawer.py
+++ lgtm/drawer.py
@@ -9,7 +9,7 @@ FONT_MIN_SIZE = 24
 
 # フォントの格納先のパスは十応環境に合わせて変更する
 # FONT_NAME = '/Library/Fonts/Arial Bold.ttf'
-FONT_NAME = '%WINDIR%\fonts'
+FONT_NAME = 'C:\Windows\Fonts\azuki.ttf'
 #FONT_NAME = /usr/share/fonts'
 
 FONT_COLOR_WHITE = (255, 255, 255, 0)
